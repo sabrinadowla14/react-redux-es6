2820ccf631892c19dd7e4dc3ff2af1be3acec6b6
Sabrina Dowla <sabrinadowla14@gmail.com>
2018-12-12 20:56:54 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+   render() {
+     return(
+        <div className="container-fluid">
+        <p>Header here ...</p>
+        {this.props.children}
+        </div>
+       );
+      }
+    }
+App.propTypes = {
+  children: PropTypes.object.isRquired
+  };
+export default App;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+   render() {
+     return(
+        <div className ="jumbotron">
+        <h1>Resume Website</h1>
+        <p>React, Redux and React Router in ES6 for ultra responcive web apps.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Cover Letter</Link>
+
+        </div>
+
+
+     );
+
+   }
+
+
+}
+export default HomePage;
